8582bcdf9a290e8b55cd1afd0e9dff9274e4ace3
china-92 <hemidovcingiz183@gmail.com>
2021-06-21 11:21:21 +0400

    porfolio

---
+++ navigation.js
@@ -0,0 +1,40 @@
+﻿document.getElementById('icon-menu').addEventListener('click', function(){
+document.getElementById('nav-list').style.right = '0'
+document.getElementById('icon-del').style.display = 'block'
+document.getElementById('icon-menu').style.display = 'none'
+})
+
+document.getElementById('icon-del').addEventListener('click', function(){
+document.getElementById('nav-list').style.right = '-3000px'
+document.getElementById('icon-del').style.display = 'none'
+document.getElementById('icon-menu').style.display = 'block'
+})
+
+
+
+
+
+const mediQuery = function (matchWidth) {
+if(matchWidth.matches){
+document.getElementById('icon-menu').style.display = 'block'
+document.getElementById('icon-del').style.display = 'none'
+
+}
+else{
+document.getElementById('icon-menu').style.display = 'none'
+document.getElementById('icon-del').style.display = 'none'
+
+}
+
+}
+
+
+const matchWidth = matchMedia('(max-width: 550px)')
+
+mediQuery(matchWidth)
+matchWidth.addEventListener('change', mediQuery)
+
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,72 @@
+﻿document.querySelector('.html').addEventListener("click", () =>{
+
+const popUpVar = document.querySelector('#popUp')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVar.style.display = 'block'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '0.3'
+}
+
+const popUpVarCSS = document.querySelector('#popUpCSS')
+const popUpVarJS = document.querySelector('#popUpJS')
+popUpVarCSS.style.display = 'none'
+popUpVarJS.style.display = 'none'
+})
+
+
+
+document.querySelector('.css').addEventListener("click", () =>{
+
+const popUpVar = document.querySelector('#popUp')
+const popUpVarJS = document.querySelector('#popUpJS')
+const popUpVarCSS = document.querySelector('#popUpCSS')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVarCSS.style.display = 'block'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '0.3'
+}
+popUpVar.style.display = 'none'
+popUpVarJS.style.display = 'none'
+})
+document.querySelector('.js').addEventListener("click", () =>{
+const popUpVar = document.querySelector('#popUp')
+const popUpVarCSS = document.querySelector('#popUpCSS')
+const popUpVarJS = document.querySelector('#popUpJS')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVarJS.style.display = 'block'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '0.3'
+}
+popUpVar.style.display = 'none'
+popUpVarCSS.style.display = 'none'
+})
+
+
+document.querySelector('.icon-del2').addEventListener("click", () =>{
+
+
+const popUpVar = document.querySelector('#popUp')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVar.style.display = 'none'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '1'
+}
+})
+document.querySelector('.icon-del2css').addEventListener("click", () =>{
+
+const popUpVar = document.querySelector('#popUpCSS')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVar.style.display = 'none'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '1'
+}
+})
+document.querySelector('.icon-del2js').addEventListener("click", () =>{
+
+const popUpVar = document.querySelector('#popUpJS')
+const toBlurVar = document.querySelectorAll('.toBlur')
+popUpVar.style.display = 'none'
+for(let i = 0; i<toBlurVar.length; i++){
+toBlurVar[i].style.opacity = '1'
+}
+})
