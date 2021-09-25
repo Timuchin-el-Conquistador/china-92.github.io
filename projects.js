9fae2f733a9eb0239d850ab7ee4bcb0664089d93
china-92 <hemidovcingiz183@gmail.com>
2021-09-25 12:15:34 +0400

    added new projects links

--- projects.js
+++ projects.js
@@ -1,18 +1,43 @@
-const sliders = document.querySelectorAll('.fa-chevron-left')
 
-let index = 1
-const sliding = () => {
-    
-    for(let i=0; i<document.querySelectorAll('.project-tile').length;i++){
-        document.querySelectorAll('.project-tile')[i].style.right ='-2000px'
+const projectContainer = document.createElement('div')
+projectContainer.style.width='70%'
 
-    }
-    document.querySelectorAll('.project-tile')[index].style.right = '10%'
-    index--
-    if(index<0){
-        index=1
-    }
+projectContainer.style.border='1px solid red'
+projectContainer.style.margin="100px auto"
 
-}
-Array.from(sliders).forEach(slider => {slider.addEventListener('click', sliding)})
+document.getElementById('projects').appendChild(projectContainer)
+const header = document.createElement('header')
+header.style.width='100%'
+header.style.display='flex'
+header.style.justifyContent='center'
+const projectTile = document.createElement('button')
+projectTile.innerHTML='General projects'
 
+header.appendChild(projectTile)
+const projectTile2 = document.createElement('button')
+projectTile2.innerHTML='React projects'
+
+header.appendChild(projectTile2)
+
+
+projectContainer.appendChild(header)
+projectContainer.style.display='none'
+projectContainer.classList.add('portfolioContainer')
+const closePortfolio = document.createElement('button')
+closePortfolio.innerHTML='CLOSE'
+header.appendChild(closePortfolio)
+
+
+
+document.getElementById('portfolio').addEventListener('click',()=>{
+  projectContainer.style.display='block'
+  
+})
+
+closePortfolio.addEventListener('click', () =>{
+  projectContainer.style.display='none'
+})
+
+
+projectContainer.appendChild(document.getElementById('survey-form'))
+projectContainer.appendChild(document.getElementById('calculator'))
